db0d69b3316df43ef7f974ac3e9bc98ce697588e
Mark D. Lattimore <mark.d.lattimore@gmail.com>
2024-12-29 09:37:51 -0500

    updated admin to work with django allauth

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,31 @@
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin # type: ignore
+from django.contrib.auth import get_user_model # type: ignore
+from django.contrib.auth.admin import UserAdmin # type: ignore
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username',]
+    list_display = [
+        "email",
+        "username",
+    ]
+
+    # Explicitly define add_fieldsets to prevent unexpected fields
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
